36aa352ccc1edac2dba84306c428e00e26621f47
samuelnihbos <partogusamuel07@gmail.com>
2022-01-19 19:52:54 +0700

    fix before change colorscheme

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -6,7 +6,7 @@ xbacklight -set 30 &
 xinput set-prop 12 309 1 &
 picom --config ~/.config/picom/picom.conf --experimental-backends &
 nitrogen --restore &
-polybar base -c ~/.config/polybar/config &
+polybar base -c ~/.config/polybar/config.ini &
 #~/.config/polybar/launch-miniblock.sh
 ncmpcpp &
 
